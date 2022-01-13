162607647d3dfbbbeef469c1d9662c1c21785912
Martin Rehberger <martin.rehberger@lipsia.digital>
2022-01-13 16:58:46 +0100

    :recycle: Make license link in plugin.php use https

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@
  * WC requires at least:    6.0.0
  *
  * License:                 GNU General Public License v3.0
- * License URI:             http://www.gnu.org/licenses/gpl-3.0.html
+ * License URI:             https://www.gnu.org/licenses/gpl-3.0.html
  */
 
 declare(strict_types=1);
